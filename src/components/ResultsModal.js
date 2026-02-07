141b76205ac0fd981762140fb2887729a0ada749
desafinadude <meerkdir@gmail.com>
2026-02-07 20:28:10 +0200

    refactor: Remove unused imports from ResultsModal component

--- src/components/ResultsModal.js
+++ src/components/ResultsModal.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import '../styles/ResultsModal.css';
 
 function ResultsModal({ score, questionsAnswered, onPlayAgain, quizType, onCoinsAwarded, roundEvents = [] }) {
